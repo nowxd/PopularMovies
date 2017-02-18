08a7cd4d722791345bc130d8760927c57d69f965
nowxd <davidcl@uchicago.edu>
2017-02-18 14:26:26 -0500

    Added an insert testcase for the content provider

---
+++ app/src/androidTest/java/org/nowxd/popularmovies/database/ContentProviderTest.java
@@ -0,0 +1,92 @@
+package org.nowxd.popularmovies.database;
+
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ContentProviderTest {
+
+    private final String TAG = ContentProviderTest.class.getSimpleName();
+
+    private Context context;
+
+    @Before
+    public void setup() {
+
+        context = InstrumentationRegistry.getTargetContext();
+        context.getContentResolver().delete(MovieContract.MovieEntry.CONTENT_URI, null, null);
+
+    }
+
+    @Test
+    public void testMovieInsert() {
+
+        // Bogus Data
+        final String MOVIE_TITLE = "movie_title1";
+        final String MOVIE_IMG_URL = "google.com";
+        final String MOVIE_PLOT = "hmm";
+        final double MOVIE_RATING = 5.5;
+        final String MOVIE_RELEASE_DATE = "1/1/2017";
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MovieContract.MovieEntry.COLUMN_TITLE, MOVIE_TITLE);
+        contentValues.put(MovieContract.MovieEntry.COLUMN_IMAGE_URL, MOVIE_IMG_URL);
+        contentValues.put(MovieContract.MovieEntry.COLUMN_PLOT, MOVIE_PLOT);
+        contentValues.put(MovieContract.MovieEntry.COLUMN_USER_RATING, MOVIE_RATING);
+        contentValues.put(MovieContract.MovieEntry.COLUMN_RELEASE_DATE, MOVIE_RELEASE_DATE);
+
+        Uri uri = context.getContentResolver().insert(MovieContract.MovieEntry.CONTENT_URI, contentValues);
+
+        long id = ContentUris.parseId(uri);
+        Assert.assertNotEquals(id, -1);
+
+        // Retrieve the Movie
+        String[] projection = {
+                MovieContract.MovieEntry._ID,
+                MovieContract.MovieEntry.COLUMN_TITLE,
+                MovieContract.MovieEntry.COLUMN_IMAGE_URL,
+                MovieContract.MovieEntry.COLUMN_PLOT,
+                MovieContract.MovieEntry.COLUMN_USER_RATING,
+                MovieContract.MovieEntry.COLUMN_RELEASE_DATE,
+        };
+
+        Cursor cursor = context.getContentResolver().query(
+                MovieContract.MovieEntry.CONTENT_URI, projection, null, null, null);
+
+        // Check the cursor has a count of 1
+        Assert.assertNotNull(cursor);
+        Assert.assertEquals(cursor.getCount(), 1);
+        Assert.assertTrue(cursor.moveToFirst());
+
+        int idIndex = cursor.getColumnIndex(MovieContract.MovieEntry._ID);
+        int titleIndex = cursor.getColumnIndex(MovieContract.MovieEntry.COLUMN_TITLE);
+        int imgIndex = cursor.getColumnIndex(MovieContract.MovieEntry.COLUMN_IMAGE_URL);
+        int plotIndex = cursor.getColumnIndex(MovieContract.MovieEntry.COLUMN_PLOT);
+        int ratingIndex = cursor.getColumnIndex(MovieContract.MovieEntry.COLUMN_USER_RATING);
+        int dateIndex = cursor.getColumnIndex(MovieContract.MovieEntry.COLUMN_RELEASE_DATE);
+
+        // Assert all the values matches with what we inserted
+        Assert.assertEquals(cursor.getLong(idIndex), id);
+        Assert.assertEquals(cursor.getString(titleIndex), MOVIE_TITLE);
+        Assert.assertEquals(cursor.getString(imgIndex), MOVIE_IMG_URL);
+        Assert.assertEquals(cursor.getString(plotIndex), MOVIE_PLOT);
+        Assert.assertEquals(cursor.getString(dateIndex), MOVIE_RELEASE_DATE);
+        double eps = 1e-10;
+        Assert.assertEquals(cursor.getDouble(ratingIndex), MOVIE_RATING, eps);
+
+        cursor.close();
+
+    }
+
+}
--- app/src/main/java/org/nowxd/popularmovies/database/MovieDbHelper.java
+++ app/src/main/java/org/nowxd/popularmovies/database/MovieDbHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class MovieDbHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "movie.db";
-    private static int DATABASE_VERSION = 1;
+    private static int DATABASE_VERSION = 2;
 
     public MovieDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -17,7 +17,7 @@ public class MovieDbHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
         final String CREATE_TABLE_SQL = "CREATE TABLE " + MovieContract.MovieEntry.TABLE_NAME + " (" +
-                MovieContract.MovieEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                MovieContract.MovieEntry._ID + " INTEGER PRIMARY KEY, " +
                 MovieContract.MovieEntry.COLUMN_TITLE + " TEXT NOT NULL, " +
                 MovieContract.MovieEntry.COLUMN_IMAGE_URL + " TEXT UNIQUE NOT NULL, " +
                 MovieContract.MovieEntry.COLUMN_PLOT + " TEXT NOT NULL, " +
@@ -38,4 +38,5 @@ public class MovieDbHelper extends SQLiteOpenHelper {
         onCreate(sqLiteDatabase);
 
     }
+
 }
